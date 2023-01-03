ff67aa51f025add2bc7a02af2b2f59362a8adb74
jimi-chhatrala <jimichhatrala8488@gmail.com>
2023-01-03 13:39:02 +0530

    Completed

---
+++ data.js
@@ -0,0 +1,83 @@
+const rowsCat = [
+  {
+    name: "all",
+  },
+  {
+    name: "mobile",
+  },
+  {
+    name: "camera",
+  },
+  {
+    name: "watch",
+  },
+  {
+    name: "shoe",
+  },
+  {
+    name: "headphone",
+  },
+];
+
+const rowsData = [
+  {
+    name: "mobile",
+    image: "mobile1",
+  },
+  {
+    name: "camera",
+    image: "camera1",
+  },
+  {
+    name: "watch",
+    image: "watch1",
+  },
+  {
+    name: "shoe",
+    image: "shoe1",
+  },
+  {
+    name: "headphone",
+    image: "headphone1",
+  },
+  {
+    name: "mobile",
+    image: "mobile2",
+  },
+  {
+    name: "camera",
+    image: "camera2",
+  },
+  {
+    name: "watch",
+    image: "watch2",
+  },
+  {
+    name: "shoe",
+    image: "shoe2",
+  },
+  {
+    name: "headphone",
+    image: "headphone2",
+  },
+  {
+    name: "mobile",
+    image: "mobile3",
+  },
+  {
+    name: "camera",
+    image: "camera3",
+  },
+  {
+    name: "watch",
+    image: "watch3",
+  },
+  {
+    name: "shoe",
+    image: "shoe3",
+  },
+  {
+    name: "headphone",
+    image: "headphone3",
+  },
+];
